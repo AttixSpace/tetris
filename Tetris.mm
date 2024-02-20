87b765c6df52bad1c41b8776ec9a3d70719cf752
Geert Fannes <geert.fannes@gmail.com>
2024-02-20 21:14:27 +0100

    Added mindmap

---
+++ Tetris.mm
@@ -0,0 +1,134 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Tetris" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1706644112135" VGAP_QUANTITY="3 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#fbf1c7" zoom="0.769">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/light_gruvbox_template.mm" followedTemplateLocation="template:/light_gruvbox_template.mm" followedMapLastTime="1699832211000" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_7974122" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_7974122" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#cc241d" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc241d"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_103960811" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc241d" TRANSPARENCY="255" DESTINATION="ID_103960811"/>
+<font SIZE="12" ITALIC="false"/>
+<edge COLOR="#cc241d"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#fdf6e3" BACKGROUND_COLOR="#282828" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#282828" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#282828" BACKGROUND_COLOR="#ebdbb2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#282828" BACKGROUND_COLOR="#d79921" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#458588" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#ffffff" BACKGROUND_COLOR="#b16286" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#ffffff" BACKGROUND_COLOR="#689d6a" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#ffffff" BACKGROUND_COLOR="#a89984" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#ebdbb2" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#ebdbb2">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#98971a">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="3 knoppen" POSITION="bottom_or_right" ID="ID_797058411" CREATED="1706644129481" MODIFIED="1706644134368">
+<node TEXT="Pull-up" ID="ID_466238476" CREATED="1706644137412" MODIFIED="1706644150073"/>
+<node TEXT="Pull-down" ID="ID_1586283497" CREATED="1706644150839" MODIFIED="1706644152754"/>
+<node TEXT="Input-pull-up" ID="ID_569498899" CREATED="1706644153346" MODIFIED="1706644157113">
+<node TEXT="Intern via Arduino" ID="ID_1708280892" CREATED="1706644157925" MODIFIED="1706644166859"/>
+</node>
+</node>
+<node TEXT="Controller" POSITION="bottom_or_right" ID="ID_903182361" CREATED="1706644135473" MODIFIED="1706644181981">
+<node TEXT="Links" ID="ID_1578478024" CREATED="1706644182953" MODIFIED="1706644186519"/>
+<node TEXT="Rechts" ID="ID_439307893" CREATED="1706644186718" MODIFIED="1706644189060"/>
+<node TEXT="Draai" ID="ID_658117466" CREATED="1706644189767" MODIFIED="1706644191703"/>
+</node>
+<node TEXT="Blokjes random kiezen" POSITION="bottom_or_right" ID="ID_710668575" CREATED="1706644195931" MODIFIED="1706644204832"/>
+<node TEXT="Herkennen rij" POSITION="bottom_or_right" ID="ID_798987982" CREATED="1706644211338" MODIFIED="1706644227804"/>
+<node TEXT="Verwijderen rij" POSITION="bottom_or_right" ID="ID_1101220514" CREATED="1706644216499" MODIFIED="1706644224333"/>
+<node TEXT="Scherm" POSITION="bottom_or_right" ID="ID_213160250" CREATED="1706644228716" MODIFIED="1706644231173">
+<node TEXT="Webshop VTIZ" ID="ID_1929116712" CREATED="1706644327849" MODIFIED="1706644336801">
+<node TEXT="8 x 32 (6euro)" ID="ID_403732749" CREATED="1706644336977" MODIFIED="1706644593489">
+<node TEXT="2 van besteld" ID="ID_1431878764" CREATED="1706646237876" MODIFIED="1706646241514"/>
+</node>
+<node TEXT="8 x 8 (3 euro)" ID="ID_1175401275" CREATED="1706644340394" MODIFIED="1706644605043"/>
+</node>
+<node TEXT="Kiwi" ID="ID_388445803" CREATED="1706644948739" MODIFIED="1706644950852">
+<node TEXT="16 x 32 (23euro)" ID="ID_833645679" CREATED="1706644420092" MODIFIED="1706644580641" LINK="https://www.kiwi-electronics.com/nl/32x16-rgb-led-matrix-paneel-6mm-pitch-3748?search=led%20matrix">
+<node TEXT="13 GPIO" ID="ID_245232294" CREATED="1706644494710" MODIFIED="1706644500142"/>
+<node TEXT="Arduino uno heeft er 14+6=20" ID="ID_1040240482" CREATED="1706644560806" MODIFIED="1706644570118"/>
+</node>
+</node>
+<node TEXT="16 x 32 met Arduino uno" ID="ID_1552962606" CREATED="1706644932393" MODIFIED="1706644944827" LINK="https://www.youtube.com/watch?v=TehnrK33CmE"/>
+</node>
+<node TEXT="Behuizing" POSITION="bottom_or_right" ID="ID_237186800" CREATED="1706644250527" MODIFIED="1706644256902">
+<node TEXT="Moet met lasercutter en 3D print" ID="ID_1884150710" CREATED="1706644257720" MODIFIED="1706644298164"/>
+<node TEXT="Ontwerpen in Solidworks" ID="ID_132539125" CREATED="1706644289188" MODIFIED="1706646494929"/>
+</node>
+<node TEXT="Blokjes" POSITION="bottom_or_right" ID="ID_887349343" CREATED="1706644316247" MODIFIED="1706644323665"/>
+<node TEXT="Arduino vastmaken en verbinden is moeilijk" POSITION="bottom_or_right" ID="ID_677054077" CREATED="1706646534964" MODIFIED="1706646550099"/>
+<node TEXT="class Board" POSITION="top_or_left" ID="ID_562895917" CREATED="1706648886078" MODIFIED="1706648894438">
+<node TEXT="16 x 32 vakjes" ID="ID_710955504" CREATED="1706648894687" MODIFIED="1706648900439"/>
+<node TEXT="gebruik niet teveel geheugen" ID="ID_44449801" CREATED="1706648913101" MODIFIED="1706648923434"/>
+<node TEXT="vakjes moeten vallen tot bodem" ID="ID_1489890480" CREATED="1706648964504" MODIFIED="1706648989667"/>
+</node>
+<node TEXT="class Blok" POSITION="top_or_left" ID="ID_478664209" CREATED="1706648935852" MODIFIED="1706648942797">
+<node TEXT="Plaats vakjes bovenaan" ID="ID_1089106198" CREATED="1706648991531" MODIFIED="1706649000156"/>
+</node>
+<node TEXT="Valsnelheid" POSITION="top_or_left" ID="ID_1836614688" CREATED="1706648943469" MODIFIED="1706648958532">
+<node TEXT="bepaald wanneer Board de vakjes moet laten vallen" ID="ID_849674053" CREATED="1706648959143" MODIFIED="1706649018521"/>
+<node TEXT="Steeds sneller en sneller" ID="ID_973671318" CREATED="1706649019023" MODIFIED="1706649027060"/>
+</node>
+<node TEXT="Score" POSITION="top_or_left" ID="ID_611973555" CREATED="1706649033481" MODIFIED="1706649035377">
+<node TEXT="Bovenste rij in binair?" ID="ID_697572412" CREATED="1706649052443" MODIFIED="1706649059788"/>
+<node TEXT="Eindscore in grote letters" ID="ID_1351737166" CREATED="1708455704248" MODIFIED="1708455716561"/>
+</node>
+<node TEXT="Eerst op computer programmeren" POSITION="top_or_left" ID="ID_1383427758" CREATED="1706649077130" MODIFIED="1706649096618"/>
+</node>
+</map>
