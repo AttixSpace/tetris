849e6f70e851896ab0fd69bfd95e9d96eed8d8fe
Geert Fannes <geert.fannes@gmail.com>
2024-03-16 20:55:58 +0100

    class Block

--- eerste_test_tetris/eerste_test_tetris.ino
+++ eerste_test_tetris/eerste_test_tetris.ino
@@ -1,64 +1,46 @@
-// C++ code
-//
 #include "LedControl.h"
+
+// LED matrix displays
 // Parameters: DIN pin, CLK pin, CS pin
 LedControl lc1 = LedControl(13, 11, 12, 4);
 LedControl lc2 = LedControl(10, 8, 9, 4);
 
-
-class Led
-{
-  public:
-    void led(int x, int y) {
-      // Zet vorige LED uit
-      lc1.setLed(a, b, c, false);
-
-      // Berken a, b en c voor nieuwe LED
-      a = y / 8;
-      int rest = y % 8;
-      b = 7 - x;
-      c = 7 - rest;
-
-      // Zet nieuwe LED aan
-      lc1.setLed(a, b, c, true);
-    }
-
-  private:
-    int a = 0;
-    int b = 0;
-    int c = 0;
-};
-Led l1;
-Led l2;
-Led l3;
-
 class Board
 {
   public:
-    void zet_led_aan(int x, int y) {
-      new_[y][x] = true;
+    void zet_led_aan(int x, int y)
+    {
+      if (0 <= x && x < 16 && 0 <= y && y < 32)
+        new_[y][x] = true;
     }
-    void show() {
-      for (int y  = 0; y < 32; y++) {
-        for (int x = 0; x < 16; x++) {
-          if (new_[y][x] != old_[y][x]) {
+    void show()
+    {
+      for (int y = 0; y < 32; y++)
+      {
+        for (int x = 0; x < 16; x++)
+        {
+          if (new_[y][x] != old_[y][x])
+          {
             set_led_(x, y, new_[y][x]);
             old_[y][x] = new_[y][x];
           }
 
-          //alle new op uit zetten.
-          if (new_[y][x] == true) {
+          // alle new op uit zetten.
+          if (new_[y][x] == true)
+          {
             new_[y][x] = false;
           }
         }
       }
     }
-  private:
 
-    void set_led_(int x, int y, bool aan) {
+  private:
+    void set_led_(int x, int y, bool aan)
+    {
       bool gebruik_lc1 = (x < 8);
 
-      if (x >= 8) {
+      if (x >= 8)
+      {
         x -= 8;
       }
 
@@ -68,10 +50,12 @@ class Board
       int b = 7 - x;
       int c = 7 - rest;
 
-      if (gebruik_lc1) {
+      if (gebruik_lc1)
+      {
         lc1.setLed(a, b, c, aan);
       }
-      else {
+      else
+      {
         lc2.setLed(a, b, c, aan);
       }
     }
@@ -80,8 +64,56 @@ class Board
     bool new_[32][16] = {};
 };
 
-Board b;
+class Block
+{
+  public:
+    void zet_positie(int x, int y)
+    {
+      pos_x = x;
+      pos_y = y;
+    }
+    void zet_steen(int i, int x, int y)
+    {
+      steen_pos_x[i] = x;
+      steen_pos_y[i] = y;
+    }
 
+    void naar_links()
+    {
+      if (pos_x > 0)
+        pos_x--;
+    }
+    void naar_rechts()
+    {
+      if (pos_x < 15)
+        pos_x++;
+    }
+    void naar_onder()
+    {
+      if (pos_y > 0)
+        pos_y--;
+    }
+
+    void show(Board &board) const
+    {
+      for (int i = 0; i < 4; ++i)
+      {
+        int x = pos_x + steen_pos_x[i];
+        int y = pos_y + steen_pos_y[i];
+        board.zet_led_aan(x, y);
+      }
+    }
+
+  private:
+    int draai = 0;
+    int pos_x = 0;
+    int pos_y = 0;
+    int steen_pos_x[4] = {};
+    int steen_pos_y[4] = {};
+};
+
+Board board;
+Block b0;
 
 const int knop1_pullup = A0;
 const int knop1_input = A1;
@@ -96,7 +128,8 @@ const int knop3_input = 7;
 
 void setup()
 {
-  for (int device = 0; device < 4; device++) {
+  for (int device = 0; device < 4; device++)
+  {
     lc1.shutdown(device, false);
     // Set the brightness to a medium value (0 is min, 15 is max)
     lc1.setIntensity(device, 8);
@@ -127,89 +160,39 @@ void setup()
   pinMode(knop3_low, OUTPUT);
   pinMode(knop3_input, INPUT_PULLUP);
   digitalWrite(knop3_low, LOW);
+
+  b0.zet_positie(8, 8);
+  b0.zet_steen(0, 0, -1);
+  b0.zet_steen(1, 0, 0);
+  b0.zet_steen(2, 1, 0);
+  b0.zet_steen(3, 1, 1);
 }
 
 void loop()
 {
-  {
-    const int knopwaarde = digitalRead(knop1_input);
-    if (knopwaarde == LOW) { //Programma voor de pull up weerstand.
-      Serial.println("De knop2 is ingedrukt.");
-      b.zet_led_aan(2, 4);
-      b.show();
-    }
+  bool do_delay = false;
+
+  if (digitalRead(knop1_input) == LOW)
+  { // Programma voor de pull up weerstand.
+    b0.naar_links();
+    do_delay = true;
   }
 
-  {
-    const int knopwaarde = digitalRead(knop2_input);
-    if (knopwaarde == HIGH) { //Programma voor de pull up weerstand.
-      Serial.println("De knop2 is ingedrukt.");
-      b.zet_led_aan(7, 17);
-      b.show();
-    }
-    else {
-      Serial.println("De knop2 is niet ingedrukt.");
-    }
+  if (digitalRead(knop3_input) == LOW)
+  { // Programma voor de input pull up weerstand.
+    b0.naar_onder();
+    do_delay = true;
   }
 
-  {
-    const int knopwaarde = digitalRead(knop3_input);
-    if (knopwaarde == LOW) { //Programma voor de pull up weerstand.
-      Serial.println("De knop2 is ingedrukt.");
-      b.zet_led_aan(15, 27);
-      b.zet_led_aan(8, 27);
-      b.show();
-    }
-    else {
-      Serial.println("De knop2 is niet ingedrukt.");
-    }
-  }
-}
-
-
-void loop_old()
-{
-  {
-    const int knopwaarde = digitalRead(knop1_input);
-    if (knopwaarde == LOW)
-    {
-      //Programma voor de pull down weerstand.
-      Serial.println("De knop1 is ingedrukt.");
-      for (int y = 0; y < 32; y++) {
-        l2.led(y % 8, y);
-        delay(10);
-      }
-    }
-    else {
-      Serial.println("De knop1 is niet ingedrukt.");
-    }
+  if (digitalRead(knop2_input) == HIGH)
+  { // Programma voor de pull down weerstand.
+    b0.naar_rechts();
+    do_delay = true;
   }
 
-  {
-    const int knopwaarde = digitalRead(knop2_input);
-    if (knopwaarde == HIGH) { //Programma voor de pull up weerstand.
-      Serial.println("De knop2 is ingedrukt.");
-      for (int x = 0; x < 32; x++) {
-        l3.led(0, x);
-        delay(20);
-      }
-    }
-    else {
-      Serial.println("De knop2 is niet ingedrukt.");
-    }
-  }
+  b0.show(board);
+  board.show();
 
-  {
-    const int knopwaarde = digitalRead(knop3_input);
-    if (knopwaarde == LOW) { //Programma voor de input pull up weerstand.
-      Serial.println("De knop3 is ingedrukt.");
-      for (int z = 0; z < 8; z++) {
-        l1.led(z % 8, 0);
-        delay(20);
-      }
-    }
-    else {
-      Serial.println("De knop3 is niet ingedrukt.");
-    }
-  }
+  if (do_delay)
+    delay(300);
 }
