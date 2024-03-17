cf2cf3758197b1daaf1245007b5edbe405811a59
Geert Fannes <geert.fannes@gmail.com>
2024-03-17 15:12:19 +0100

    Gebruik van Nederlandse namen Automatisch aanmaken en zakken van een blok Nieuwe class Spel

--- eerste_test_tetris/eerste_test_tetris.ino
+++ eerste_test_tetris/eerste_test_tetris.ino
@@ -5,7 +5,7 @@
 LedControl lc1 = LedControl(13, 11, 12, 4);
 LedControl lc2 = LedControl(10, 8, 9, 4);
 
-class Board
+class Scherm
 {
   public:
     void zet_led_aan(int x, int y)
@@ -13,7 +13,7 @@ class Board
       if (0 <= x && x < 16 && 0 <= y && y < 32)
         new_[y][x] = true;
     }
-    void show()
+    void toon()
     {
       for (int y = 0; y < 32; y++)
       {
@@ -64,7 +64,7 @@ class Board
     bool new_[32][16] = {};
 };
 
-class Block
+class Blok
 {
   public:
     void zet_positie(int x, int y)
@@ -94,40 +94,167 @@ class Block
         pos_y--;
     }
 
-    void show(Board &board) const
+    bool staat_onderaan() const
+    {
+      return pos_y == 0;
+    }
+
+    void toon_op(Scherm &scherm) const
     {
       for (int i = 0; i < 4; ++i)
       {
         int x = pos_x + steen_pos_x[i];
         int y = pos_y + steen_pos_y[i];
-        board.zet_led_aan(x, y);
+        scherm.zet_led_aan(x, y);
       }
     }
 
   private:
-    int draai = 0;
+    int draaiing = 0;
     int pos_x = 0;
     int pos_y = 0;
     int steen_pos_x[4] = {};
     int steen_pos_y[4] = {};
 };
 
-Board board;
-Block b0;
+class Spel
+{
+  public:
+    void setup()
+    {
+      setup_knoppen();
+    }
 
-const int knop1_pullup = A0;
-const int knop1_input = A1;
-const int knop1_low = A2;
+    void loop()
+    {
+      behandel_blok();
 
-const int knop2_high = 3;
-const int knop2_input = 4;
-const int knop2_low = 5;
+      behandel_knoppen();
 
-const int knop3_low = 6;
-const int knop3_input = 7;
+      blok.toon_op(scherm);
+      scherm.toon();
+
+      if (knop_werd_ingeduwd)
+        delay(300);
+    }
+
+  private:
+    void behandel_blok()
+    {
+      // Verwijder blok als die helemaal onderaan staat
+      if (blok_bestaat)
+      {
+        if (blok.staat_onderaan())
+          blok_bestaat = false;
+      }
+
+      // Maak blok aan als die nog niet bestaat
+      if (!blok_bestaat)
+      {
+        maak_random_blok();
+        blok.zet_positie(8, 30);
+        blok_bestaat = true;
+      }
+
+      // Laat blok zakken als het tijd is om hem te laten zakken
+      if (millis() >= laat_blok_zakken)
+      {
+        blok.naar_onder();
+        const unsigned long val_snelheid = 300;
+        laat_blok_zakken += val_snelheid;
+      }
+    }
+
+    void behandel_knoppen()
+    {
+      knop_werd_ingeduwd = false;
+
+      if (digitalRead(knop1_input) == LOW)
+      { // Programma voor de pull up weerstand.
+        blok.naar_onder();
+        knop_werd_ingeduwd = true;
+      }
+
+      if (digitalRead(knop3_input) == LOW)
+      { // Programma voor de input pull up weerstand.
+        blok.naar_rechts();
+        knop_werd_ingeduwd = true;
+      }
+
+      if (digitalRead(knop2_input) == HIGH)
+      { // Programma voor de pull down weerstand.
+        blok.naar_links();
+        knop_werd_ingeduwd = true;
+      }
+    }
+
+    void maak_random_blok()
+    {
+      const long aantal_blokken = 2;
+      const long welke_blok = random(aantal_blokken);
+      if (welke_blok == 0)
+      {
+        blok.zet_steen(0, 0, -1);
+        blok.zet_steen(1, 0, 0);
+        blok.zet_steen(2, 1, 0);
+        blok.zet_steen(3, 1, 1);
+      }
+      else if (welke_blok == 1)
+      {
+        blok.zet_steen(0, 0, 0);
+        blok.zet_steen(1, 0, 1);
+        blok.zet_steen(2, 1, 0);
+        blok.zet_steen(3, 1, 1);
+      }
+    }
+
+    void setup_knoppen()
+    {
+      pinMode(knop1_pullup, OUTPUT);
+      pinMode(knop1_input, INPUT);
+      pinMode(knop1_low, OUTPUT);
+      digitalWrite(knop1_pullup, HIGH);
+      digitalWrite(knop1_low, LOW);
+
+      pinMode(knop2_high, OUTPUT);
+      pinMode(knop2_input, INPUT);
+      pinMode(knop2_low, OUTPUT);
+      digitalWrite(knop2_high, HIGH);
+      digitalWrite(knop2_low, LOW);
+
+      pinMode(knop3_low, OUTPUT);
+      pinMode(knop3_input, INPUT_PULLUP);
+      digitalWrite(knop3_low, LOW);
+    }
+
+    const int knop1_pullup = A0;
+    const int knop1_input = A1;
+    const int knop1_low = A2;
+
+    const int knop2_high = 3;
+    const int knop2_input = 4;
+    const int knop2_low = 5;
+
+    const int knop3_low = 6;
+    const int knop3_input = 7;
+
+    Scherm scherm;
+
+    Blok blok;
+    bool blok_bestaat = false;
+
+    bool knop_werd_ingeduwd = false;
+
+    // milliseconds wanneer blok moet zakken
+    unsigned long laat_blok_zakken = 0;
+};
+
+Spel spel;
 
 void setup()
 {
+  Serial.begin(9600);
+
   for (int device = 0; device < 4; device++)
   {
     lc1.shutdown(device, false);
@@ -143,56 +270,10 @@ void setup()
     lc2.clearDisplay(device);
   }
 
-  Serial.begin(9600);
-
-  pinMode(knop1_pullup, OUTPUT);
-  pinMode(knop1_input, INPUT);
-  pinMode(knop1_low, OUTPUT);
-  digitalWrite(knop1_pullup, HIGH);
-  digitalWrite(knop1_low, LOW);
-
-  pinMode(knop2_high, OUTPUT);
-  pinMode(knop2_input, INPUT);
-  pinMode(knop2_low, OUTPUT);
-  digitalWrite(knop2_high, HIGH);
-  digitalWrite(knop2_low, LOW);
-
-  pinMode(knop3_low, OUTPUT);
-  pinMode(knop3_input, INPUT_PULLUP);
-  digitalWrite(knop3_low, LOW);
-
-  b0.zet_positie(8, 8);
-  b0.zet_steen(0, 0, -1);
-  b0.zet_steen(1, 0, 0);
-  b0.zet_steen(2, 1, 0);
-  b0.zet_steen(3, 1, 1);
+  spel.setup();
 }
 
 void loop()
 {
-  bool do_delay = false;
-
-  if (digitalRead(knop1_input) == LOW)
-  { // Programma voor de pull up weerstand.
-    b0.naar_onder();
-    do_delay = true;
-  }
-
-  if (digitalRead(knop3_input) == LOW)
-  { // Programma voor de input pull up weerstand.
-    b0.naar_rechts();
-    do_delay = true;
-  }
-
-  if (digitalRead(knop2_input) == HIGH)
-  { // Programma voor de pull down weerstand.
-    b0.naar_links();
-    do_delay = true;
-  }
-
-  b0.show(board);
-  board.show();
-
-  if (do_delay)
-    delay(300);
+  spel.loop();
 }
